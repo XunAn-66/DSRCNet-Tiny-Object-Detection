6415c310bbd0ec607cb574308f72d1c0a56e5d22
XunAn-66 <anxun@stu.ynu.edu.cn>
2026-03-28 15:12:21 +0800

    Add files via upload

---
+++ train.py
@@ -0,0 +1,21 @@
+import warnings
+import matplotlib
+
+matplotlib.use('Agg')
+warnings.filterwarnings('ignore')
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    model = YOLO('ultralytics/cfg/models/v8/yolov8s-edgetest.yaml')
+    model.train(data='../dataset/data.yaml',
+                cache=False,
+                imgsz=640,
+                epochs=300,
+                batch=4,
+                close_mosaic=40,
+                workers=0,
+                optimizer='SGD',
+                patience=20,
+                project='runs/train',
+                name='exp',
+                )
